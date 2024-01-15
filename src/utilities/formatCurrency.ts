dabcc6ea09fe8550e1afd3096f75a77d9390304b
Deniz Saganak <dilara.deniz.saganak@accenture.com>
2024-01-15 17:13:40 +0100

    create basis of the project

---
+++ src/utilities/formatCurrency.ts
@@ -0,0 +1,6 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat('de-DE', 
+    {currency: "EUR", style: "currency"})
+
+export function formatCurrency(number: number) {
+    return CURRENCY_FORMATTER.format(number);
+}
